305661b373a309ef180786889ffb40eb087505fb
Bhoumesh Kumar Dutta <bhoumeshdutta@gmail.com>
2025-10-16 15:07:43 +0530

    First Commit

---
+++ Day 1.py
@@ -0,0 +1,116 @@
+#Hello This is my first code of python
+"""This is a multi line comment
+I am learning python programming language for data analytics
+I am very excited to learn python"""
+"""variables"""
+
+# sher = "harsh bhaiya"
+
+# SheryiansSchool = "students" #pascal case
+
+# sheryiansSchool = "students" #camel case
+
+# sheryians_school = "students" #snake case
+
+
+
+
+"""data types"""
+
+a = -34
+
+b = 56.8
+c = 12/3
+
+v = 34j
+
+print(type(v))
+
+
+
+st = '1231243235 dsagaiogiaeb !@#$%^&*'
+
+print(type(st))
+
+b = True
+
+t = False
+
+print(type(b))
+
+
+"""Strings and Type Conversion"""
+
+a = "😊"
+print(ord(a))
+
+b = 65
+print(chr(b))
+name = "harsh"
+age = 20
+
+print(f"my name is {name} and my age is {age}")
+
+age = int(input("hello what is your age"))
+
+print(age)
+a = 5
+b = 32
+
+
+print(a + b)
+print(b - a)
+print(a * b)
+print(b//a)
+print(b/a)
+print(5**100)
+print(32%5)
+
+
+print(12+4/2)
+
+#assignment operators 
+
+a = 23
+
+#compound assignmet operations
+
+a = 20
+
+a += 20
+a += 40
+a += 60
+a-= 55
+a*= 66
+a/= 32
+a//= 445
+a**= 220022
+
+print(a)
+
+a = 12.1
+b = 12 
+
+print(a == b)
+
+print(a != b)
+
+print(a > b)
+print(45 < 67)
+print(23 >= 23)
+print(45 <= 45)
+
+
+print(ord("A"))
+print(ord("B"))
+
+print("ABC" > "ACD")
+
+
+print(12 >20 and 123 > 100 and 34 == 34 and 45 < 90)
+
+print(12 !=12 or 23 ==45 or 67 == 56 or 10 > 5)
+
+print(not 12 == 12)
+ 
+ 
